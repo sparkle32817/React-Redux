dc94be5f8736aad840abd4233f33181d97cb9dda
sparkle32817 <54101792+sparkle32817@users.noreply.github.com>
2020-02-19 22:45:00 +0700

    Understanding about Redux

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FilterLink from './containers/FilterLink'
+import { VisibilityFilters } from './actions/index'
+
+const Footer = () => {
+  return (
+    <div>
+      <FilterLink filter={VisibilityFilters.SHOW_ALL}>ALL</FilterLink>
+      <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+      <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/reducers/todos.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const todos = (state = [], action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => (
+          console.log(todo),
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+        )        
+      )
+    default:
+      return state
+  }
+}
+
+export default todos
